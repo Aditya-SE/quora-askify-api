1dbf8025145a55f6f89da30b9c31ba03d1e53c41
Aditya-SE <adityapatil849703801313@gmail.com>
2025-11-01 15:43:14 +0530

    file added

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine" , "ejs");
+
+app.set("views" , path.join(__dirname, "/views"));
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({extended : true}));
+
+app.get("/" , (req , res) =>{
+    res.send("server working fine")
+})
+
+app.listen(port , () =>{
+    console.log(`server is listening on port : ${port}`);
+});
