4efef0bd223184f72700e22057a02081e80863c4
Aditya-SE <adityapatil849703801313@gmail.com>
2025-11-03 09:28:42 +0530

    file added

--- index.js
+++ index.js
@@ -10,9 +10,28 @@ app.set("views" , path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({extended : true}));
 
-app.get("/" , (req , res) =>{
-    res.send("server working fine")
-})
+let posts = [
+    {
+        username : "adi",
+        content : "consistent"
+    },
+    {
+        username : "nikhil",
+        content : "work smartly"
+    },
+    {
+        username : "naveen",
+        content : "work hard"
+    }
+]
+
+app.get("/posts" , (req , res) =>{
+    res.render("index.ejs" , { posts });
+});
+
+app.get("/posts/new" , (req , res) =>{
+    res.render("new.ejs");
+});
 
 app.listen(port , () =>{
     console.log(`server is listening on port : ${port}`);
